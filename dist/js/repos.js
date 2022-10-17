3e22446605bcce249065e33b2fc2098b11e4f708
Little Human <little.human@novonine.space>
2022-10-18 03:56:13 +0700

    Update repo list
    Signed-off-by: Little Human <little.human@novonine.space>

--- dist/js/repos.js
+++ dist/js/repos.js
@@ -1,22 +1,42 @@
 const kongkowRepos = [
   {
-    full_name: "kongkowitpku/.github",
+    full_name: "KongkowITPekanbaru/.github",
     name: ".github",
   },
   {
-    full_name: "kongkowitpku/assets",
-    name: "assets",
+    full_name: "KongkowITPekanbaru/awesome-riau-project",
+    name: "awesome-riau-project",
   },
   {
-    full_name: "kongkowitpku/dns",
+    full_name: "KongkowITPekanbaru/bayaraja",
+    name: "bayaraja",
+  },
+  {
+    full_name: "KongkowITPekanbaru/dns",
     name: "dns",
   },
   {
-    full_name: "kongkowitpku/issues-browser",
+    full_name: "KongkowITPekanbaru/event-driven-poc",
+    name: "event-driven-poc",
+  },
+  {
+    full_name: "KongkowITPekanbaru/issues-browser",
     name: "issues-browser",
   },
   {
-    full_name: "kongkowitpku/soon",
+    full_name: "KongkowITPekanbaru/masjid-network",
+    name: "masjid-network",
+  },
+  {
+    full_name: "KongkowITPekanbaru/paypoint",
+    name: "paypoint",
+  },
+  {
+    full_name: "KongkowITPekanbaru/soon",
     name: "soon",
+  },
+  {
+    full_name: "KongkowITPekanbaru/space",
+    name: "space",
   },
 ];
